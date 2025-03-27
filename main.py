3a221e149f2bd72d8b6fbd0e45edcd0d40712866
showhwa <103583170+showhwa@users.noreply.github.com>
2025-03-27 04:01:18 +0000

    update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import requests
 
 # https://git.unlock-music.dev/um/lib_um_crypto_rust/archive/main.tar.gz
 # https://git.unlock-music.dev/um/cli/archive/main.tar.gz
+# https://git.unlock-music.dev/um/testsuite/archive/master.tar.gz
 
 # /um/kgg-dec/stars
 
@@ -18,14 +19,19 @@ for project in projects:
     print(project)
     code = requests.head(url).status_code
     if code == 404:
+        url = f"https://git.unlock-music.dev/um/{project}/archive/master.tar.gz"
+        code = requests.head(url).status_code
+
+    if code == 404:
         print(project + "404\n")
         with open("error_log.txt", "a", encoding="utf-8") as file:
             file.write(f"{project}: 404\n")
     else:
-        if not os.path.isdir("code"):
-            os.mkdir("code")
-        download = requests.get(url)
-        download.raise_for_status()
-        with open(f"code/{project}.tar.gz", 'wb') as file:
-            file.write(download.content)
-        print(f"{project}已下载到当前目录: code/{project}.tar.gz")
+        pass
+        # if not os.path.isdir("code"):
+        #     os.mkdir("code")
+        # download = requests.get(url)
+        # download.raise_for_status()
+        # with open(f"code/{project}.tar.gz", 'wb') as file:
+        #     file.write(download.content)
+        # print(f"{project}已下载到当前目录: code/{project}.tar.gz")
